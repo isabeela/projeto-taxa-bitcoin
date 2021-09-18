b9c049019fb97770abb44d5bd70c73157cc33833
isabeela <isabeelabg@gmail.com>
2021-09-17 21:12:11 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { BitcoinService } from './bitcoin.service';
+import { TemporizadorService } from './temporizador.service';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  providers: [BitcoinService, TemporizadorService],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/temporizador.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { BitcoinService } from './bitcoin.service';
+
+@Injectable()
+export class TemporizadorService {
+  constructor(public BitcoinService: BitcoinService) {}
+
+  private timer: any;
+  private counter = 0;
+
+  /*inicia a contagem em ms a cada n ms o contador é zerado */
+  start() {
+    this.BitcoinService.update();
+    if (!this.timer) {
+      this.timer = setInterval(() => {
+        this.counter++;
+        if (this.counter == 15) {
+          this.BitcoinService.update();
+          this.counter = 0;
+        }
+      }, 1000);
+    }
+  }
+
+  getCount() {
+    return this.counter;
+  }
+}
