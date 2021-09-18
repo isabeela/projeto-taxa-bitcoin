acb14ed1aa92d33300511e1391e7f0db0564f721
isabeela <isabeelabg@gmail.com>
2021-09-17 21:14:05 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 import { BitcoinService } from './bitcoin.service';
 import { TemporizadorService } from './temporizador.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, NavbarComponent],
   bootstrap: [AppComponent],
   providers: [BitcoinService, TemporizadorService],
 })
